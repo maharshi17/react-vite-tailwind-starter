f980504e08c5c65cc5f11312ba5d50292c4cbfd8
Maharshi Pandya <maharshipandya1999@gmail.com>
2026-01-04 01:26:12 -0500

    added new folders

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="min-h-screen p-3">
+    <div className="min-h-screen">
       <Outlet />
     </div>
   )
